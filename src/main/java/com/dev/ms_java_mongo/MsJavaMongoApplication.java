b01f16d94b0874df52a1c3198719b25f69688747
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2025-05-26 23:39:11 -0300

    feat: add project

---
+++ src/main/java/com/dev/ms_java_mongo/MsJavaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ms_java_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJavaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJavaMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/ms_java_mongo/MsJavaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ms_java_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJavaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
